12bcdaddd5aa21ca38568108462e8a4498073c4f
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2023-10-10 22:15:07 +0800

    base64解码(不理解为什么之前不需要也能成功)

--- src/apple_shortcut_handler.py
+++ src/apple_shortcut_handler.py
@@ -5,17 +5,20 @@ def handler(event: dict, context: dict) -> dict:
     """处理lambda请求，并返回链接"""
     headers = event["headers"]
     if "image" not in headers.get("content-type"):
-        return {"statusCode": 400, "body": "content-type must be image/jpeg"}
+        return {"statusCode": 400, "body": "content-type must be image"}
     try:
         filename = headers["filename"]
         fileext = headers["fileext"]
-        filecontent = event["data"]
+        filecontent = event["body"]
     except Exception as e:
         return {"statusCode": 400, "body": f"parse body error: {str(e)}"}
     # {year}/{month}/{year}{month}{day}-{random}-{filename}{.suffix}
     import datetime
     import random
+    import base64
 
+    filecontent = filecontent.encode("utf-8")
+    filecontent = base64.b64decode(filecontent)
     now = datetime.datetime.now()
     try:
         path = f"{now.year}/{now.month}/{now.year}-{now.month}-{now.day}-{random.randint(1000, 9999)}-{filename}.{fileext}"
